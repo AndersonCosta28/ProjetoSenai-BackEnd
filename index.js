3184b3fdf165ebd245aa1019220a59f8071d9abf
Mert1s <andgtsc@gmail.com>
2021-12-12 14:50:14 -0300

    Upper na pessoa

--- index.js
+++ index.js
@@ -95,8 +95,8 @@ app.post('/evento',(request,response)=> {
     console.log(req)
     const sql = `insert into evento (banda,dia,horario,ingresso_inteira,ingresso_meia) values (Upper('${req.banda}'), '${req.Dia}', '${req.Horario}', ${req.valor_inteira}, ${req.valor_meia})`;
     pool.query(sql)
-    .then(res => { console.log(sql);console.log("Deu Certo"); response.send(true) })
-    .catch(e => { console.log(e); response.send(false) })
+    .then(res => { console.log("Deu Certo"); response.send(true) })
+    .catch(e => { console.log(sql);console.log(e); response.send(false) })
 })
 
 const PORT = process.env.PORT || 8080;
